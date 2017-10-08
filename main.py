b3f6ae1a580f357da0919bb2a2cac2721e370291
Harshwardhan Jain <hj364@cornell.edu>
2017-10-08 11:52:34 -0400

    Started structure

---
+++ main.py
@@ -0,0 +1,29 @@
+import urllib as ul
+import random as rand
+
+websites = ["www.apple.com",
+			"www.wikipedia.org",
+			"www.youtube.com",
+			"www.yahoo.com",
+			"www.instagram.com",
+			"www.marketwatch.com",
+			"www.amazon.com",
+
+			"www.espnfc.com"]
+
+def get_next_website()
+	
+def visit_website(url):
+	ul.urlopener(url)
+	ul.read()
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
